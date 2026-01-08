b93090d7f22abefae55783aaba357b6b0445cdd2
GaryMortimer-sch <gary.mortimer@cliftonprep.org.za>
2026-01-08 11:46:44 +0200

    Delete services directory

--- services/audioService.ts
+++
@@ -1,3 +0,0 @@
-
-// Audio Service removed as per user request to not use Gemini API and use native TTS.
-export {};
--- services/geminiService.ts
+++
@@ -1,3 +0,0 @@
-
-// Gemini Service removed as per user request to not use Gemini API.
-export {};
